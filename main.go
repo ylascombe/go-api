cb80664de65fbd3361c6f753158e3f58f752155c
Yohan Lascombe <ylascombe@octo.com>
2017-05-03 00:50:09 +0200

    WIP version

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 
    "github.com/gorilla/mux"
    //"github.com/ghodss/yaml"
+	"encoding/json"
 )
 
 type Manifest struct {
    formatVersion    string `json:"format_version" yaml:"format_version"`
-   reactivePlatform ReactivePlatform `json:"reactive_platform" yaml:"reactive_platform"`
-   applications []Application `json:"applications" yaml:"applications"`
+   //reactivePlatform ReactivePlatform `json:"reactive_platform" yaml:"reactive_platform"`
+   //applications []Application `json:"applications" yaml:"applications"`
 }
 
 type ReactivePlatform struct {
@@ -51,15 +52,33 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func api(w http.ResponseWriter, r *http.Request) {
 
-   file, err := os.Open("manifest.yml") // For read access.
+   file, err := os.Open("manifest.json") // For read access.
    if err != nil {
        log.Fatal(err)
    }
 
-   data := make([]byte, 100)
+   data := make([]byte, 1000)
    count, err := file.Read(data)
    if err != nil {
        log.Fatal(err)
    }
    fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
+	var config Manifest
+//	err = yaml.Unmarshal(data[:count], &config)
+//	if err != nil {
+//		fmt.Printf("err: %v\n", err)
+//		return
+//	}
+//	fmt.Println(config)
+//	fmt.Println(config.formatVersion)
+
+	data = []byte(`{"format_version": "0.1"}`)
+
+	count = len(data)
+	if err := json.Unmarshal(data[:count], &config); err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	fmt.Println(config)
 }
