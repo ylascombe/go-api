92975bde60fa03e6b0232784a7885c848d7614e4
Yohan Lascombe <ylascombe@octo.com>
2017-05-03 17:23:21 +0200

    TDD setup

--- main.go
+++ main.go
@@ -1,90 +1,89 @@
 package main
 
 import (
-   "fmt"
-   "html"
-   "log"
-   "net/http"
-   "os"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
 
-   "github.com/gorilla/mux"
-   //"github.com/ghodss/yaml"
-   "gopkg.in/yaml.v2"
+	//"github.com/ghodss/yaml"
+	"gopkg.in/yaml.v2"
 	//"encoding/json"
+	"github.com/gorilla/mux"
 )
 
 type Manifest struct {
-   FormatVersion    string `json:"format_version" yaml:"format_version"`
-   ReactPlatform ReactivePlatform `json:"reactive_platform" yaml:"reactive_platform"`
-   Applications []Application `json:"applications" yaml:"applications"`
+	FormatVersion string           `json:"format_version" yaml:"format_version"`
+	ReactPlatform ReactivePlatform `json:"reactive_platform" yaml:"reactive_platform"`
+	Applications  []Application    `json:"applications" yaml:"applications"`
 }
 
 type ReactivePlatform struct {
-   Version string `json:"version" yaml:"version"`
-   ExtraVars  string    `json:"extra_vars" yaml:"extra_vars"`
+	Version   string `json:"version" yaml:"version"`
+	ExtraVars map[string]string `json:"extra_vars" yaml:"extra_vars"`
 }
 
 type Application struct {
-   Name string `json:"name" yaml:"name"`
-   Spark Spark `json:"spark" yaml:"spark"`
-   Api Api `json:"api" yaml:"api"`
-
+	Name  string `json:"name" yaml:"name"`
+	Spark Spark  `json:"spark" yaml:"spark"`
+	Api   Api    `json:"api" yaml:"api"`
 }
 
 type Spark struct {
-
+	//Version   string `json:"version" yaml:"version"`
+	//ExtraVars string `json:"extra_vars" yaml:"extra_vars"`
 }
 
 type Api struct {
-
+	//Version   string `json:"version" yaml:"version"`
+	//ExtraVars string `json:"extra_vars" yaml:"extra_vars"`
 }
 
 func main() {
 
-   router := mux.NewRouter().StrictSlash(true)
-   router.HandleFunc("/", Index)
-   router.HandleFunc("/reac", api)
-   log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/reac", api)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-   fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
 
-   file, err := os.Open("manifest.yml") // For read access.
-   if err != nil {
-       log.Fatal(err)
-   }
+	file, err := os.Open("manifest.yml") // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
 
-   data := make([]byte, 1000)
-   count, err := file.Read(data)
-   if err != nil {
-       log.Fatal(err)
-   }
-   fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	data := make([]byte, 1000)
+	_, err = file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	//config := unmarshall(data[:count])
 
-   var config Manifest
-   err = yaml.Unmarshal(data[:count], &config)
-   if err != nil {
-   	fmt.Printf("err: %v\n", err)
-   	return
-   }
-   fmt.Println(config)
-   //fmt.Println(config.FormatVersion)
-   //fmt.Println(config.ReactPlatform.Version)
-   fmt.Println(config.Applications[0].Name)
+	//data = []byte(`{"format_version": "0.1"}`)
 
-   //data = []byte(`{"format_version": "0.1"}`)
-
-   //count = len(data)
-   //if err := json.Unmarshal(data[:count], &config); err != nil {
-   //	fmt.Printf("err: %v\n", err)
-   //	return
-   //}
-   //fmt.Println("resultat: \n", config)
+	//count = len(data)
+	//if err := json.Unmarshal(data[:count], &config); err != nil {
+	//	fmt.Printf("err: %v\n", err)
+	//	return
+	//}
+	//fmt.Println("resultat: \n", config)
 }
 
+func unmarshall(yamlText []byte) *Manifest {
+	var config Manifest
+	var err = yaml.Unmarshal(yamlText, &config)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return nil
+	}
 
-func
+	return &config
+}
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+const YAML = `format_version: 0.1
+
+reactive_platform:
+  version: 3.1.2
+  extra_vars:
+    var1: value1
+    var2: value2
+
+applications:
+  - name: colis360
+    spark:
+      version: 1.4.2
+      extra_vars: []
+    api:
+     version: 1.4.3
+     extra_vars: toto`
+
+func TestParseManifest(t *testing.T) {
+	manifest := unmarshall([]byte(YAML))
+	assert.Equal(t, "0.1", manifest.FormatVersion)
+	assert.Equal(t, 1, len(manifest.Applications))
+	//assert.Equal(t, "1.4.2", len(manifest.Applications[0].Spark.Version))
+}
+func TestParseReactivePlatform(t *testing.T) {
+	manifest := unmarshall([]byte(YAML))
+	assert.Equal(t, "3.1.2", manifest.ReactPlatform.Version)
+	assert.Equal(t, 2, len(manifest.ReactPlatform.ExtraVars))
+	assert.Equal(t, "value1", manifest.ReactPlatform.ExtraVars["var1"])
+	assert.Equal(t, "value2", manifest.ReactPlatform.ExtraVars["var2"])
+}
