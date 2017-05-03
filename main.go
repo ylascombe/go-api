f02dca61db6a4dadea33fdaddf0c545f3441a139
Yohan Lascombe <ylascombe@octo.com>
2017-05-03 16:27:23 +0200

    YAML parse start to work

--- main.go
+++ main.go
@@ -9,24 +9,25 @@ import (
 
    "github.com/gorilla/mux"
    //"github.com/ghodss/yaml"
-	"encoding/json"
+   "gopkg.in/yaml.v2"
+	//"encoding/json"
 )
 
 type Manifest struct {
-   formatVersion    string `json:"format_version" yaml:"format_version"`
-   //reactivePlatform ReactivePlatform `json:"reactive_platform" yaml:"reactive_platform"`
-   //applications []Application `json:"applications" yaml:"applications"`
+   FormatVersion    string `json:"format_version" yaml:"format_version"`
+   ReactPlatform ReactivePlatform `json:"reactive_platform" yaml:"reactive_platform"`
+   Applications []Application `json:"applications" yaml:"applications"`
 }
 
 type ReactivePlatform struct {
-   Version string `json:"version" yaml:"version"` // Affects YAML field names too.
-   ExtraVars  int    `json:"extra_vars" yaml:"extra_vars"`
+   Version string `json:"version" yaml:"version"`
+   ExtraVars  string    `json:"extra_vars" yaml:"extra_vars"`
 }
 
 type Application struct {
-   name string `json:"name" yaml:"name"`
-   spark Spark `json:"spark" yaml:"spark"`
-   api Api `json:"api" yaml:"api"`
+   Name string `json:"name" yaml:"name"`
+   Spark Spark `json:"spark" yaml:"spark"`
+   Api Api `json:"api" yaml:"api"`
 
 }
 
@@ -52,7 +53,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func api(w http.ResponseWriter, r *http.Request) {
 
-   file, err := os.Open("manifest.json") // For read access.
+   file, err := os.Open("manifest.yml") // For read access.
    if err != nil {
        log.Fatal(err)
    }
@@ -64,21 +65,26 @@ func api(w http.ResponseWriter, r *http.Request) {
    }
    fmt.Printf("read %d bytes: %q\n", count, data[:count])
 
-	var config Manifest
-//	err = yaml.Unmarshal(data[:count], &config)
-//	if err != nil {
-//		fmt.Printf("err: %v\n", err)
-//		return
-//	}
-//	fmt.Println(config)
-//	fmt.Println(config.formatVersion)
+   var config Manifest
+   err = yaml.Unmarshal(data[:count], &config)
+   if err != nil {
+   	fmt.Printf("err: %v\n", err)
+   	return
+   }
+   fmt.Println(config)
+   //fmt.Println(config.FormatVersion)
+   //fmt.Println(config.ReactPlatform.Version)
+   fmt.Println(config.Applications[0].Name)
 
-	data = []byte(`{"format_version": "0.1"}`)
+   //data = []byte(`{"format_version": "0.1"}`)
 
-	count = len(data)
-	if err := json.Unmarshal(data[:count], &config); err != nil {
-		fmt.Printf("err: %v\n", err)
-		return
-	}
-	fmt.Println(config)
+   //count = len(data)
+   //if err := json.Unmarshal(data[:count], &config); err != nil {
+   //	fmt.Printf("err: %v\n", err)
+   //	return
+   //}
+   //fmt.Println("resultat: \n", config)
 }
+
+
+func
