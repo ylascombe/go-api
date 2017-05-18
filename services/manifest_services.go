0b072bd37d949962b7b7046b1c070602dd08ef1e
ylascombe <yohan.lascombe-prestataire@colis.laposte.fr>
2017-05-18 18:34:05 +0200

    Export hard coded var into const

--- services/manifest_services.go
+++ services/manifest_services.go
@@ -7,26 +7,26 @@ import (
 
 const ANSIBLE_PLAYBOOK  = "ansible-playbook -i inventories/"
 
-func BuildCommands(target string, manifest string, path_to_vault_password_file string) []string {
-	config, _ := utils.UnmarshallFromFile(config.MANIFEST_FILE)
+func BuildCommands(target string, manifestUrl string, path_to_vault_password_file string) []string {
+	manifest, _ := utils.UnmarshallFromFile(config.MANIFEST_FILE)
 
 	var ansibleCommands = []string {}
-	ansibleCommands = append(ansibleCommands, "cd /home/ylascombe/projects/init-poste-dev")
+	ansibleCommands = append(ansibleCommands, "cd " + config.INIT_POSTE_DEV_PATH)
 	ansibleCommands = append(ansibleCommands, "pwd")
 	ansibleCommands = append(ansibleCommands, ANSIBLE_PLAYBOOK + target + " plateforme_reactive.yml")
 	ansibleCommands = append(ansibleCommands, ANSIBLE_PLAYBOOK + target + " plateforme_reactive.yml")
 
-	for i:=0; i<len(config.Applications); i++ {
+	for i:=0; i<len(manifest.Applications); i++ {
 
-		application := config.Applications[i]
+		application := manifest.Applications[i]
 		appName := application.Name
-		if config.Applications[i].Spark.Version != ""  {
+		if manifest.Applications[i].Spark.Version != ""  {
 			ansibleCommands = append(ansibleCommands, ANSIBLE_PLAYBOOK + target + " exploit_download_nexus_artifact.yml -e artifact_group=fr.laposte.colis." + appName + " -e artifact=" + application.Spark.ArtifactName + " -e repository=releases -e version=" + application.Spark.Version)
 
 			ansibleCommands = append(ansibleCommands, ANSIBLE_PLAYBOOK + target + " deploy_spark_app.yml -e spark_app_name=" + application.Spark.ArtifactName + " -e spark_app_version=" + application.Spark.Version + " -e spark_app_filename=" + application.Spark.ArtifactName + "-" + application.Spark.Version + " -assembly.jar -e force_deploy=true --vault-password-file " + path_to_vault_password_file)
 
 		}
-		if config.Applications[i].Api.Version != "" {
+		if manifest.Applications[i].Api.Version != "" {
 			ansibleCommands = append(ansibleCommands, ANSIBLE_PLAYBOOK + target + " deploy_apiserver.yml -e apiserver_instance=" + application.Api.ArtifactName +" -e apiserver_version=" + application.Api.Version +" --vault-password-file " + path_to_vault_password_file)
 
 
--- services/manifest_services_test.go
+++ services/manifest_services_test.go
@@ -9,15 +9,15 @@ import (
 func TestBuildCommand(t *testing.T) {
 	commands := BuildCommands("LOCAL.ini", "", "fake_path")
 
-	assert.Equal(t, 7, len(commands))
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini plateforme_reactive.yml", commands[0])
+	assert.Equal(t, 10, len(commands))
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini plateforme_reactive.yml", commands[3])
 
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini exploit_download_nexus_artifact.yml -e artifact_group=fr.laposte.colis.colis360 -e artifact=spark-colis-360 -e repository=releases -e version=1.4.2", commands[1])
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_spark_app.yml -e spark_app_name=spark-colis-360 -e spark_app_version=1.4.2 -e spark_app_filename=spark-colis-360-1.4.2 -assembly.jar -e force_deploy=true --vault-password-file fake_path", commands[2])
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_apiserver.yml -e apiserver_instance=api-colis-360 -e apiserver_version=1.4.3 --vault-password-file fake_path", commands[3])
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini exploit_download_nexus_artifact.yml -e artifact_group=fr.laposte.colis.colis360 -e artifact=spark-colis-360 -e repository=releases -e version=1.4.2", commands[4])
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_spark_app.yml -e spark_app_name=spark-colis-360 -e spark_app_version=1.4.2 -e spark_app_filename=spark-colis-360-1.4.2 -assembly.jar -e force_deploy=true --vault-password-file fake_path", commands[5])
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_apiserver.yml -e apiserver_instance=api-colis-360 -e apiserver_version=1.4.3 --vault-password-file fake_path", commands[6])
 
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini exploit_download_nexus_artifact.yml -e artifact_group=fr.laposte.colis.datasafe -e artifact=spark-datasafe -e repository=releases -e version=0.0.7", commands[4])
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_spark_app.yml -e spark_app_name=spark-datasafe -e spark_app_version=0.0.7 -e spark_app_filename=spark-datasafe-0.0.7 -assembly.jar -e force_deploy=true --vault-password-file fake_path", commands[5])
-	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_apiserver.yml -e apiserver_instance=api-datasafe -e apiserver_version=0.0.8 --vault-password-file fake_path", commands[6])
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini exploit_download_nexus_artifact.yml -e artifact_group=fr.laposte.colis.datasafe -e artifact=spark-datasafe -e repository=releases -e version=0.0.7", commands[7])
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_spark_app.yml -e spark_app_name=spark-datasafe -e spark_app_version=0.0.7 -e spark_app_filename=spark-datasafe-0.0.7 -assembly.jar -e force_deploy=true --vault-password-file fake_path", commands[8])
+	assert.Equal(t, "ansible-playbook -i inventories/LOCAL.ini deploy_apiserver.yml -e apiserver_instance=api-datasafe -e apiserver_version=0.0.8 --vault-password-file fake_path", commands[9])
 
 }
